5e11c3af5e88a5041f431f96e290dcf4f83d6c40
Cédric Roy <cecric.roy@broadsign.com>
2022-12-02 12:29:52 -0500

    Day 2 part 2

--- advent_of_code_2022/day_2.py
+++ advent_of_code_2022/day_2.py
@@ -2,8 +2,8 @@ from advent_of_code_2022.common import get_data_from_file
 
 
 def calculate_move_score(me):
-    # (1 for Rock, 2 for Paper, and 3 for Scissors)
-    score = {"X": 1, "Y": 2, "Z": 3}
+
+    score = {"rock": 1, "paper": 2, "scissor": 3}
     return score.get(me)
 
 
@@ -17,38 +17,57 @@ def is_winning(me, opponnent):
 
 
 def calculate_winning_score(opponent, me):
-    opponent_converted_value = {"A": "rock", "B": "paper", "C": "scissor"}.get(opponent)
-    me_converted_value = {"X": "rock", "Y": "paper", "Z": "scissor"}.get(me)
 
-    if is_tied_score(me_converted_value, opponent_converted_value):
+    if is_tied_score(me, opponent):
         return 3
-    return 6 if is_winning(me_converted_value, opponent_converted_value) else 0
+    return 6 if is_winning(me, opponent) else 0
+
+
+def part_1_translation(me, opponent):
+    me_converted_value = {"X": "rock", "Y": "paper", "Z": "scissor"}.get(me)
+    return me_converted_value
+
+
+def part_2_translation(me, opponent):
+    winning_data = {
+        "rock": "scissor",
+        "paper": "rock",
+        "scissor": "paper",
+    }
+    loosing_data = dict([(v, k) for k, v in winning_data.items()])
+    if me == "Y":
+        return opponent
+
+    return winning_data[opponent] if me == "X" else loosing_data[opponent]
 
 
 def is_tied_score(me_converted_value, opponent_converted_value):
     return opponent_converted_value == me_converted_value
 
 
-def game_score(game_data):
+def game_score(game_data, translator):
     opponent, me = game_data.split(" ")
-    my_score = calculate_move_score(me)
-    winning_score = calculate_winning_score(opponent, me)
+    opponent_converted_value = {"A": "rock", "B": "paper", "C": "scissor"}.get(opponent)
+    me_converted_value = translator(me, opponent_converted_value)
+    my_score = calculate_move_score(me_converted_value)
+    winning_score = calculate_winning_score(opponent_converted_value, me_converted_value)
     return my_score + winning_score
 
 
-def get_total_score(games_data):
+def get_total_score(games_data, translator):
     list_of_game_data = games_data.split("\n")
-    results = [game_score(x) for x in list_of_game_data]
+    results = [game_score(x, translator) for x in list_of_game_data]
 
     return sum(results)
 
 
-def main(filename):
+def main(filename, translator):
     file_data = get_data_from_file(filename)
-    score = get_total_score(file_data)
+    score = get_total_score(file_data, translator)
 
     print(f"The total score is {score}")
 
 
 if __name__ in "__main__":
-    main("day_2.txt")
+    main("day_2.txt", part_1_translation)
+    main("day_2.txt", part_2_translation)
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -1,15 +1,19 @@
 from unittest import mock
 
-from advent_of_code_2022.day_2 import get_total_score, is_winning, main
+from advent_of_code_2022.day_2 import get_total_score, is_winning, main, part_1_translation, part_2_translation
 
 
 def test_game_score():
     games_data = """A Y
 B X
 C Z"""
-    total_score = get_total_score(games_data)
+    total_score = get_total_score(games_data, part_1_translation)
 
     assert total_score == 15
+
+    total_score = get_total_score(games_data, part_2_translation)
+
+    assert total_score == 12
 
 
 def test_winning():
@@ -30,7 +34,13 @@ def test_main(
     joined_raw_data = "\n".join([str(_) for _ in data])
     mocked_get_data_from_file.return_value = joined_raw_data
     mocked_get_total_score.return_value = "SCORE"
-    main("filename.txt")
+    main("filename.txt", part_1_translation)
     mocked_get_data_from_file.assert_called_once_with("filename.txt")
-    mocked_get_total_score.assert_called_once_with(joined_raw_data)
+    mocked_get_total_score.assert_called_once_with(joined_raw_data, part_1_translation)
     mocked_print.assert_called_once_with("The total score is SCORE")
+
+
+def test_part_2_translation():
+    assert part_2_translation("Y", "rock") == "rock"
+    assert part_2_translation("X", "paper") == "rock"
+    assert part_2_translation("Z", "scissor") == "rock"
